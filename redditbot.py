98cfb39f6da43a5d25936c501c32567238eefa0c
Chau Luu <cvqluu@gmail.com>
2021-03-21 21:11:56 +0000

    added saving to disk of completed mentions

--- redditbot.py
+++ redditbot.py
@@ -1,4 +1,6 @@
+import os
 import time
+import sys
 from collections import OrderedDict
 
 import praw
@@ -6,24 +8,32 @@ import spacy
 from praw.models import Comment
 
 from argmatcher import ArgMatcher
+from local_info import USER_INFO
 
+def read_completed_list(file):
+    completed = []
+    with open(file) as fp:
+        for line in fp:
+            completed.append(line.strip())
+    return completed
 
 class MentionsBot:
 
-    def __init__(self, argmatch):
+    def __init__(self, argmatch, user_info):
         self.reddit = praw.Reddit(
-                    client_id="XXXXX",
-                    client_secret="XXXX",
-                    password="XXXXX",
-                    user_agent="vegan response bot",
-                    username="animalsupportbot",
-                    check_for_async=False
+                    check_for_async=False,
+                    **user_info
                 )
         self.inbox = praw.models.Inbox(self.reddit, _data={})
         self.argmatch = argmatch
         self.completed = []
+        self.completed_file = './completed.csv'
+        if os.path.isfile(self.completed_file):
+            self.completed = read_completed_list(self.completed_file)
+
         self.response_template = '{}\n\nThis was an automatically generated response that I matched to the idea that "{}"'
-        self.end_message = '\n`This was an automatically generated response based on the idea(s)/myth(s):`\n `{}`'
+        self.end_message = '\n`This was an automatically generated response based on the idea(s)/myth(s):`\n `{}` \n \
+                (Responses taken from vegan advocates like Earthling Ed)'
     
     def clear_already_replied(self):
         """
@@ -39,6 +49,13 @@ class MentionsBot:
                         reply_authors = [r.author for r in replies]
                         if 'animalsupportbot' in reply_authors:
                             self.completed.append(mention)
+                            self.append_to_completed(mention)
+
+    def append_to_completed(self, comment_id):
+        with open(self.completed_file, 'a') as wp:
+            line = '{}\n'.format(comment_id)
+            wp.write(line)
+
 
     def format_response_persentence(self, resps):
         """
@@ -81,8 +98,9 @@ class MentionsBot:
                         response_text = resp[3]
                         parent.reply(self.response_template.format(response_text, resp[0]))
                         self.completed.append(mention)
+                        self.append_to_completed(mention)
 
-    def reply_mentions_persentence(self, limit=25):
+    def reply_mentions_persentence(self, limit=None):
         """
         Main functionality. Go through mentions and reply to parent comments
         Uses persentence argmatcher
@@ -97,23 +115,27 @@ class MentionsBot:
                         formatted_response = self.format_response_persentence(resps)
                         parent.reply(formatted_response)
                         self.completed.append(mention)
+                        self.append_to_completed(mention)
+                        print(formatted_response)
     
     
     def run(self, refresh_rate=600):
         self.clear_already_replied()
         while True:
             self.reply_mentions_persentence()
+            print('Replied to mentions, sleeping for {} seconds...'.format(refresh_rate))
             time.sleep(refresh_rate)
 
 
 if __name__ == "__main__":
+    refresh_rate = int(sys.argv[1])
     nlp = spacy.load('en_core_web_lg')
     nlp.add_pipe('universal_sentence_encoder', config={'model_name':'en_use_lg'})
 
     argm = ArgMatcher(nlp, None, None, preload=True)
-    mb = MentionsBot(argm)
+    mb = MentionsBot(argm, USER_INFO)
 
-    mb.run(refresh_rate=600)
+    mb.run(refresh_rate=refresh_rate)
 
 
 
